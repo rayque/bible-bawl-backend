ddd1dc1a3bbc0dd6ff351a760593944a5042704e
rayque <rayque.oliveira@gmail.com>
2019-11-04 10:25:37 -0400

    Corrigindo resolver

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,20 +1,20 @@
-const { User } = require('./../../models')
+const { Usuario } = require('./../../models')
 
 module.exports = {
   usuarios() {
-    return User
+    return Usuario
     .findAll()
     .then( users => {
-      return users.map(user => {         
+      return users.map(user => {
         return {
           id: user.id,
           nome: user.nome,
           email: user.email,
         }
-      })        
+      })
     })
     .catch(err => {
-      console.log(err);      
+      console.log(err);
     })
   }
 }
