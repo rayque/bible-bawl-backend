f5ce284cedd7c53702a3220f94de4aef89f553cc
rayque <rayque.oliveira@gmail.com>
2019-11-05 13:28:07 -0400

    cadastrando equipe e participante

--- src/graphql/resolvers/Mutations/EquipeMutation.js
+++ src/graphql/resolvers/Mutations/EquipeMutation.js
@@ -8,24 +8,49 @@ module.exports = {
       transaction = await Equipe.sequelize.transaction();
       const data = dados.dados || {};
 
-      const promises =  Object.keys(data).map(async key => {
+      // Add Categoria
+
+
+      // Cadastro da Equipe
+      const nomes = [];
+      for (let i = 0; i < data.length; i++) {
+        const words = data[i].nome.split(" ");
+        nomes.push(words[0]);
+      }
+      const nomeEquipe = nomes.join(' ');
+      const equipe = await Equipe.create({nome: nomeEquipe});
+
+      // Cadastro dos Participantes
+      const promises = Object.keys(data).map(async key => {
         return Participante.create({
           nome: data[key].nome,
           data_nascimento: data[key].data_nascimento,
+          equipe_id: equipe.id,
         });
       });
 
-      const participantes = await Promise.all(promises);
+      const allParticipantes = await Promise.all(promises);
 
-           
-
-      // CRIAR EQUIPE E RETORNAR
+      const participantes = allParticipantes.map(part => {
+        return {
+          id: part.id,
+          nome: part.nome,
+          data_nascimento: part.data_nascimento,
+        }
+      });
 
       await transaction.commit();
 
-    } catch (err){
+      return {
+        id: equipe.id,
+        nome: equipe.nome,
+        participantes
+      };
+
+    } catch (err) {
       if (transaction) await transaction.rollback();
-      throw new Error("Erro ao cadastrar Respondedor");
+      throw new Error(err);
     }
   }
 }
+
