7ec43ef3eb97d5a2da761ecd315629b180810acf
rayque <rayque.oliveira@gmail.com>
2019-11-06 16:54:53 -0400

    add mode categoia

---
+++ src/database/migrations/20191106204619-create-equipe.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Equipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      categoria_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Equipes');
+  }
+};
--- src/models/equipe.js
+++ src/models/equipe.js
@@ -1,18 +1,22 @@
+'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Equipe = sequelize.define('Equipe', {
-      nome: DataTypes.STRING,
-    },
-    {
-      freezeTableName: true,
-      // define the table's name
-      tableName: 'equipes',
+    nome: DataTypes.STRING,
+    categoria_id: DataTypes.INTEGER
+  }, {
+    freezeTableName: true,
+    // define the table's name
+    tableName: 'equipes',
+  });
+  Equipe.associate = function(models) {
+    Equipe.hasMany(models.Participante, {
+      foreignKey: 'equipe_id',
+      as: 'participantes'
     });
-    Equipe.associate = function(models) {
-      Equipe.hasMany(models.Participante, {
-        foreignKey: 'equipe_id',
-        as: 'participantes'
-      });
-    };
-
+    Equipe.belongsTo(models.Categoria, {
+      foreignKey: 'categoria_id',
+      as: 'categoria'
+    });
+  };
   return Equipe;
 };
