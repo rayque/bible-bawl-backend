2f29b87a2e63ab9cc5096960d57d3e018c657e8f
rayque <rayque.oliveira@gmail.com>
2019-11-28 16:16:06 -0400

    add get Respondedor

--- src/graphql/resolvers/Queries/RespondedorQuery.js
+++ src/graphql/resolvers/Queries/RespondedorQuery.js
@@ -6,7 +6,9 @@ module.exports = {
       .findAll({include: ['equipes']})
       .then(respondedores => {
         return respondedores.map(resp => {
+
           console.log(resp.equipes.length);
+
           const equipes = resp.equipes.map(equipe => {
             return {
               id: equipe.id,
@@ -22,7 +24,58 @@ module.exports = {
         })
       })
       .catch(() => {
-        throw new Error('Erro ao buscar usuários');
+        throw new Error('Erro ao buscar respondedorees');
+      });
+  },
+  getRespondedor(_, {id}) {
+    return Respondedor
+      .findByPk(id, {
+        include: [
+          {association: 'equipes',  include: ['participantes', 'categoria']},
+        ]
+      })
+      .then(respondedor => {
+
+        const equipes = respondedor.equipes.map(equipe => {
+
+          const cat = equipe.categoria;
+          let categoria = null;
+          if (cat) {
+            categoria = {
+              id: cat.id,
+              nome: cat.nome,
+              descricao: cat.descricao,
+              idade_min: cat.idade_min,
+              idade_max: cat.idade_max,
+            };
+          }
+
+          const participantes = equipe.participantes.map(participante => {
+            return {
+              id: participante.id,
+              nome: participante.nome,
+            }
+          });
+
+          return {
+            id: equipe.id,
+            nome: equipe.nome,
+            categoria,
+            participantes
+          }
+        });
+
+        return {
+          id: respondedor.id,
+          nome: respondedor.nome,
+          cod_acesso: respondedor.cod_acesso,
+          equipes
+        }
+
+      })
+      .catch(e => {
+        console.log(e);
+        throw new Error('Erro ao buscar respondedor');
       });
   },
 };
