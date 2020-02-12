d0eb052320294e3fd1eceef3b055897d3192a03e
rayque <rayque.oliveira@gmail.com>
2020-02-12 16:23:19 -0400

    removendo validação

--- src/config/context.js
+++ src/config/context.js
@@ -3,13 +3,20 @@ const pubsub = new PubSub();
 const jwt = require('jsonwebtoken');
 
 module.exports = async ({req, connection}) => {
-    let auth = null;
+    let token;
+    // if (typeof connection === "object") {
+    //     token =  connection.context.headers && connection.context.headers.authorization;
+    // }
 
-    if (connection) {
-        auth = connection.context.headers.authorization
+    if (req) {
+        token = req.headers.authorization;
     }
-    auth = auth || req.headers.authorization
-    const token = auth && auth.substring(7);
+
+    // console.log(token.length > 0);
+
+    // token = token.length > 0 ? token.substring(7) : null;
+
+    // token = token && token.substring(7);
 
     let auxiliar = false;
     let admin = false;
@@ -17,47 +24,25 @@ module.exports = async ({req, connection}) => {
     if (token) {
         let payloadToken = jwt.decode(token, process.env.APP_JWT_AUTH_SECRET);
 
-            // console.clear()
-            // console.log()
-            // console.log("------------------- CONTEXT -------------------");
-            // console.log()
-            // console.table(payloadToken);
-
-
-
-
-
-        if (new Date(payloadToken.exp * 1000) > new Date()) {
-            auxiliar = !!payloadToken.respondedorId || auxiliar;
-            admin = !!payloadToken.userId || admin;
-        } else {
-            // throw new Error("Sessão Expirou");
-        }
+        auxiliar = !!payloadToken.respondedorId || auxiliar;
+        admin = !!payloadToken.userId || admin;
     }
 
-    // console.log(auxiliar, admin);
+    // console.log(auxiliar);
 
     const err = new Error('Acesso negado!');
 
     return {
         pubsub,
         validarAdmin() {
-            // console.log()
-            // console.log("==================== validarAdmin ====================");
-            // console.table({admin: admin});
-            // console.log()
+            return;
             if (!admin) throw err;
         },
         validarIsLogged() {
-            // console.log();
-            // console.log("==================== validarIsLogged ====================");
-            // console.log()
-            // console.table({admin: admin,auxiliar: auxiliar});
-            // console.log();
-
-            if (admin) return;
-            if (auxiliar) return;
-            throw err;
+            return
+            // if (admin) return;
+            // if (auxiliar) return;
+            // throw err;
         }
     };
 };
