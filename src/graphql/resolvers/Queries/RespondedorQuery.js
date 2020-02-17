193b56d0ad46fd5773dc8dc099c7584f5777ffd7
rayque <rayque.oliveira@gmail.com>
2020-02-17 17:01:08 -0400

    add respostas do participante ao type particopante

--- src/graphql/resolvers/Queries/RespondedorQuery.js
+++ src/graphql/resolvers/Queries/RespondedorQuery.js
@@ -1,85 +1,118 @@
-const { Respondedor } = require('./../../../models');
+const {Respondedor, Pergunta} = require('./../../../models');
 
 module.exports = {
-  getRespondedores(_, args, context) {
-    context.validarAdmin();
-    return Respondedor
-      .findAll({include: ['equipes']})
-      .then(respondedores => {
-        return respondedores.map(resp => {
-          const equipes = resp.equipes.map(equipe => {
-            return {
-              id: equipe.id,
-              nome: equipe.nome
+    getRespondedores(_, args, context) {
+        context.validarAdmin();
+        return Respondedor
+            .findAll({include: ['equipes']})
+            .then(respondedores => {
+                return respondedores.map(resp => {
+                    const equipes = resp.equipes.map(equipe => {
+                        return {
+                            id: equipe.id,
+                            nome: equipe.nome
+                        }
+                    });
+                    return {
+                        id: resp.id,
+                        nome: resp.nome,
+                        cod_acesso: resp.cod_acesso,
+                        equipes
+                    }
+                })
+            })
+            .catch(() => {
+                throw new Error('Erro ao buscar respondedorees');
+            });
+    },
+    async getRespondedor(_, {id}, context) {
+        context.validarIsLogged();
+
+        const perguntaAtual = await Pergunta.findOne(
+            {
+                where: {pergunta_atual: 1},
+                attributes: ['id']
             }
-          });
-          return {
-            id: resp.id,
-            nome: resp.nome,
-            cod_acesso: resp.cod_acesso,
-            equipes
-          }
-        })
-      })
-      .catch(() => {
-        throw new Error('Erro ao buscar respondedorees');
-      });
-  },
-  getRespondedor(_, {id}, context) {
-    context.validarIsLogged();
-    return Respondedor
-      .findByPk(id, {
-        include: [
-          {association: 'equipes',  include: ['participantes', 'categoria']},
-        ]
-      })
-      .then(respondedor => {
+        );
 
-        const equipes = respondedor.equipes.map(equipe => {
+        return Respondedor
+            .findByPk(id, {
+                include: [
+                    {
+                        association: 'equipes',
+                        // include: ['participantes', 'categoria'],
+                        include: [
+                            {
+                                association: 'participantes',
+                                include: [
+                                    {association: 'perguntas'}
+                                ],
+                            },
+                            {association: 'categoria'}
+                        ],
 
-          const cat = equipe.categoria;
-          let categoria = null;
-          if (cat) {
-            categoria = {
-              id: cat.id,
-              nome: cat.nome,
-              descricao: cat.descricao,
-              idade_min: cat.idade_min,
-              idade_max: cat.idade_max,
-            };
-          }
+                    },
+                ]
+            })
+            .then(respondedor => {
 
-          const sigla = equipe.participantes.map(participante => {
-            return participante.nome.charAt(0);
-          }).join('');
+                const equipes = respondedor.equipes.map(equipe => {
 
+                    const cat = equipe.categoria;
+                    let categoria = null;
+                    if (cat) {
+                        categoria = {
+                            id: cat.id,
+                            nome: cat.nome,
+                            descricao: cat.descricao,
+                            idade_min: cat.idade_min,
+                            idade_max: cat.idade_max,
+                        };
+                    }
 
-          const participantes = equipe.participantes.map(participante => {
-            return {
-              id: participante.id,
-              nome: participante.nome,
-            }
-          });
+                    const sigla = equipe.participantes.map(participante => {
+                        return participante.nome.charAt(0);
+                    }).join('');
 
-          return {
-            id: equipe.id,
-            nome: equipe.nome,
-            sigla,
-            categoria,
-            participantes
-          }
-        });
+                    const participantes = equipe.participantes.map(participante => {
 
-        return {
-          id: respondedor.id,
-          nome: respondedor.nome,
-          cod_acesso: respondedor.cod_acesso,
-          equipes
-        }
+                        let respostas = [];
+                        const pergunta = participante.perguntas.filter(pergunta => {
+                            return pergunta.id === perguntaAtual.id;
+                        });
 
-      })
-      .catch(e => {
-        throw new Error('Erro ao buscar respondedor');
-      });
-  },
+                        respostas.push({
+                            pergunta: perguntaAtual.id,
+                            resposta: pergunta.length ? pergunta[0].ParticipantePergunta.resposta : false,
+                        })
+
+                        return {
+                            id: participante.id,
+                            nome: participante.nome,
+                            respostas
+                        };
+                    });
+
+                    return {
+                        id: equipe.id,
+                        nome: equipe.nome,
+                        sigla,
+                        categoria,
+                        participantes
+                    }
+                });
+
+                return {
+                    id: respondedor.id,
+                    nome: respondedor.nome,
+                    cod_acesso: respondedor.cod_acesso,
+                    equipes
+                }
+
+            })
+            .catch(e => {
+                console.log(e);
+                throw new Error('Erro ao buscar respondedor');
+            });
+    },
 };
