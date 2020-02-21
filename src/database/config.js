eec80b84872599970cfec5122860488f24373f20
rayque <rayque.oliveira@gmail.com>
2020-02-21 13:28:30 -0400

    add seed remover respostas

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,15 @@
 /* Gerar todas as seed para copa fake */
-const copaFakeSeed = false;
+const copaFakeSeed = true;
 
 /* Gerar seed  somente auxiliares e equipes */
-const auxiliaresSeed = false;
-const equipesSeed = false;
+const auxiliaresSeed = true;
+const equipesSeed = true;
 
 const runSeed = {
     auxiliares: copaFakeSeed || auxiliaresSeed,
     equipes: copaFakeSeed || equipesSeed,
     copaFake: copaFakeSeed,
+    deletarRespostas: true,
 };
 
 module.exports = runSeed;
